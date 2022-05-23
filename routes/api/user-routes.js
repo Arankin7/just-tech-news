6fb44350b1d19b2d04c705d26c0cb8176a723030
AnthonyR <arankin789@gmail.com>
2022-05-23 14:05:24 -0400

    used hashing to verify password login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,33 @@ router.post('/', (req, res) =>{
     });
 });
 
+router.post('/login', (req, res) =>{
+    // expects {emai: '', password: ''}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData =>{
+        if(!dbUserData){
+            res.status(400).json({message: 'No user found with this email address'});
+            return;
+        }
+        // res.json({user: dbUserData});
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword){
+            res.status(400).json({
+                message: 'Incorrect Password'
+            });
+            return;
+        }
+
+        res.json({user: dbUserData, message: 'You are now logged in!'});
+    }) ;
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) =>{
     // expects {username: '', email: '', password, ''}
